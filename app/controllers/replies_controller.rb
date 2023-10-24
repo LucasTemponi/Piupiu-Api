2a80b645e6ba74559e53e8c2c5a3567961cb0ace
Lucas Temponi <lucastemponi@pop-os.localdomain>
2023-10-24 08:17:20 -0300

    feat(QueryOptimization): removes puts

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -15,9 +15,7 @@ class RepliesController < PostsController
   def show
     @post = Post.find(params[:id])
     total = @post.replies.count
-    puts ["\n\n =========== Current user:  ", @current_user]
     current_user_likes = Like.where(user_id: @current_user.id).pluck(:post_id)
-    puts ["\n\n =========== ", current_user_likes]
     replies = @post.replies.order(created_at: :desc).map { |reply|
       reply.create_post_return_structure(current_user_likes)
   }
